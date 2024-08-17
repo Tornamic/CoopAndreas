7c9550208dbe1c87dd45ff8b7c96dd70e82bc253
Tornamic <kirilltymoshchenko59@gmail.com>
2024-08-17 13:47:43 +0300

    add nicknames above waypoints

--- project_files/CNetworkPlayerWaypoint.cpp
+++ project_files/CNetworkPlayerWaypoint.cpp
@@ -16,5 +16,14 @@ void CNetworkPlayerWaypoint::Process()
 		CRadar::TransformRadarPointToScreenSpace(screen, radar);
 
 		CRadar::DrawRadarSprite(eRadarSprite::RADAR_SPRITE_WAYPOINT, screen.x, screen.y, 255);
+
+		if (FrontEndMenuManager.m_bDrawRadarOrMap) 
+		{
+			screen.x = CUtil::SCREEN_STRETCH_X(screen.x) - CUtil::SCREEN_STRETCH_X(8.0f) / 2.f * 6; // 8.0f - size of the sprite, / 2 - half of the size sprite, * 6 - half of the font size
+			screen.y = CUtil::SCREEN_STRETCH_Y(screen.y) - CUtil::SCREEN_STRETCH_X(8.0f) / 2.f * 5; // 5 - font size / 2 - 1
+
+			CRadar::LimitToMap(&screen.x, &screen.y);
+			CDXFont::Draw(std::floor(screen.x), std::floor(screen.y), player->GetName(), D3DCOLOR_RGBA(181, 24, 24, 255));
+		}
 	}
 }
