4f8d0872a5c46b7d460f4002df1134b6b43a2bab
Tornamic <kirilltymoshchenko59@gmail.com>
2024-08-31 10:51:23 +0300

    sync repo

--- source/Main.cpp
+++ source/Main.cpp
@@ -19,12 +19,29 @@ public:
 			{
 				CDebugVehicleSpawner::Process();
 
+				if (GetAsyncKeyState(VK_F8) && FindPlayerPed(0)->m_pVehicle)
+				{
+					Sleep(300);
+					CStreaming::RequestModel(1010, eStreamingFlags::GAME_REQUIRED);
+					CStreaming::LoadAllRequestedModels(false);
+					CStreaming::RequestVehicleUpgrade(1010, eStreamingFlags::GAME_REQUIRED);
+
+					char count = 10;
+					while (!CStreaming::HasVehicleUpgradeLoaded(1010) && count)
+					{
+						Sleep(5);
+						count--;
+					}
+
+					plugin::Command<Commands::ADD_VEHICLE_MOD>(CPools::GetVehicleRef(FindPlayerPed(0)->m_pVehicle), 1010);
+				}
+
 				if (CNetwork::m_bConnected)
 				{
 
 					CPlayerPed* localPlayer = FindPlayerPed(0);
 
-					int syncRate = 40;
+					int syncRate = 30;
 					CVector velocity{};
 
 					bool isInVehicle = localPlayer->m_pVehicle && localPlayer->m_pVehicle->m_pDriver == localPlayer;
@@ -32,7 +49,7 @@ public:
 
 					if (velocity.x == 0 && velocity.y == 0 && velocity.z == 0)
 					{
-						syncRate = 100;
+						syncRate = 60;
 					}
 
 					if (GetTickCount() > (isInVehicle ? lastDriverSyncTickRate : lastOnFootSyncTickRate) + syncRate)
@@ -50,7 +67,7 @@ public:
 						}
 					}
 
-					if (GetTickCount() > lastIdleVehicleSyncTickRate + 250)
+					if (GetTickCount() > lastIdleVehicleSyncTickRate + 100)
 					{
 						CNetworkVehicleManager::UpdateIdle();
 						lastIdleVehicleSyncTickRate = GetTickCount();
