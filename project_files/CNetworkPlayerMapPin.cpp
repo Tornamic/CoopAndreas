ceb369088ecae04e7ab6db8fb2a4606368ca4997
Vladimir <68227685+NoPressF@users.noreply.github.com>
2025-01-07 18:09:29 +0100

    Some refactored code & See detail
    - Implemented rotation of the player icon on the radar when in a vehicle to improve orientation and display of the direction of travel

--- project_files/CNetworkPlayerMapPin.cpp
+++ project_files/CNetworkPlayerMapPin.cpp
@@ -17,15 +17,32 @@ CVector2D GetPlayerMarkerPosition()
 
 	CVector2D ret{};
 	
-
 	CRadar::TransformRadarPointToScreenSpace(ret, rotatedPos);
 
 	return ret;
 };
 
+float CalculateMarkerAngle(CNetworkPlayer* player)
+{
+	float baseAngle = player->m_lOnFoot->rotation;
+
+	if (player->m_pPed->m_pVehicle != nullptr)
+	{
+		baseAngle = player->m_pPed->m_pVehicle->GetHeading();
+	}
+
+	if (!FrontEndMenuManager.m_bDrawRadarOrMap)
+	{
+		return baseAngle - CRadar::m_fRadarOrientation - (float)M_PI;
+	}
+	else
+	{
+		return baseAngle - CRadar::m_fRadarOrientation + (float)M_PI;
+	}
+}
+
 void CNetworkPlayerMapPin::Process()
 {
-
 	for (auto player : CNetworkPlayerManager::m_pPlayers)
 	{
 		CWorld::PlayerInFocus = player->GetInternalId();
@@ -33,37 +50,19 @@ void CNetworkPlayerMapPin::Process()
 		if (CWorld::PlayerInFocus == -1 || player->m_lOnFoot == nullptr)
 			continue;
 
-		if (!FrontEndMenuManager.m_bDrawRadarOrMap)
-		{
-			CVector2D pos = GetPlayerMarkerPosition();
-			float angle = player->m_lOnFoot->rotation - CRadar::m_fRadarOrientation - 3.141592f;
+		CVector2D pos = GetPlayerMarkerPosition();
+		float angle = CalculateMarkerAngle(player);
 
-			CRadar::DrawRotatingRadarSprite(
-				&CRadar::RadarBlipSprites[RADAR_SPRITE_CENTRE],
-				pos.x,
-				pos.y,
-				angle,
-				5 * RsGlobal.maximumWidth / 640,
-				5 * RsGlobal.maximumHeight / 360,
-				player->m_pPed->IsHidden() ? CRGBA{ 50, 50, 50, 255 } : CRGBA{ 255, 255, 255, 255 }
-			);
-		}
-		else
-		{
-			CVector2D pos = GetPlayerMarkerPosition();
-
-			float angle = player->m_lOnFoot->rotation + 3.141592f;
-
-			CRadar::DrawRotatingRadarSprite(
-				&CRadar::RadarBlipSprites[RADAR_SPRITE_CENTRE],
-				pos.x,
-				pos.y,
-				angle,
-				5 * RsGlobal.maximumWidth / 640,
-				5 * RsGlobal.maximumHeight / 360,
-				player->m_pPed->IsHidden() ? CRGBA{ 50, 50, 50, 255 } : CRGBA{ 255, 255, 255, 255 }
-			);
-		}
+		CRadar::DrawRotatingRadarSprite(
+			&CRadar::RadarBlipSprites[RADAR_SPRITE_CENTRE],
+			pos.x,
+			pos.y,
+			angle,
+			5 * RsGlobal.maximumWidth / 640,
+			5 * RsGlobal.maximumHeight / 360,
+			player->m_pPed->IsHidden() ? CRGBA{ 50, 50, 50, 255 } : CRGBA{ 255, 255, 255, 255 }
+		);
 	}
+
 	CWorld::PlayerInFocus = 0;
 }
