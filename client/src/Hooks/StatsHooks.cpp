b43b2134727b3f3fdd790521a1ee54153b9bae12
진짜 Claude 속도 <rosto.bayou_9f@icloud.com>
2026-06-27 11:01:03 -0300

    Fix weapon type constants in StatsHooks.cpp

--- client/src/Hooks/StatsHooks.cpp
+++ client/src/Hooks/StatsHooks.cpp
@@ -20,7 +20,7 @@ int8_t __fastcall CPed__GetWeaponSkill_Hook(CPed* This, SKIP_EDX, eWeaponType we
 	int reqStatLevel2;
 	int reqStatLevel1;
 
-	if (weaponType < WEAPON_PISTOL || weaponType > WEAPON_TEC9)
+	if (weaponType < WEAPONTYPE_PISTOL || weaponType > WEAPONTYPE_TEC9)
 	{
 		return 1;
 	}
@@ -40,7 +40,7 @@ int8_t __fastcall CPed__GetWeaponSkill_Hook(CPed* This, SKIP_EDX, eWeaponType we
 		reqStatLevel1 = CWeaponInfo::GetWeaponInfo(weaponType, 1)->m_nReqStatLevel;
 		return reqStatLevel1 <= CStats::GetStatValue(SkillStatIndex);
 	}
-	if (weaponType == WEAPON_PISTOL && This->m_nPedType == PED_TYPE_COP)
+	if (weaponType == WEAPONTYPE_PISTOL && This->m_nPedType == PED_TYPE_COP)
 	{
 		return 3;
 	}
@@ -79,7 +79,7 @@ void StatsHooks::InjectHooks()
 	patch::RedirectJump(0x5E3B60, CPed__GetWeaponSkill_Hook);
 	patch::RedirectCall(0x5A8357, CPed__Dress_Hook);
 
-	const std::vector<int> CStats__SetStatValue_Refs = {
+	const std::vector<uintptr_t> CStats__SetStatValue_Refs = {
 		0x00439117, 0x00439157, 0x00439194, 0x0043919D, 0x00439937, 0x00439947, 0x00439953, 0x0043995F, 0x0043996B, 0x00439977,
 		0x00439983, 0x0043998F, 0x0043999B, 0x004399AA, 0x004399B6, 0x004399C2, 0x004399DA, 0x004399E9, 0x004399F8, 0x00439A07,
 		0x0044230E, 0x00443E80, 0x00443EC2, 0x0044AE79, 0x00478EBC, 0x00480D3D, 0x00480DDA, 0x00489D73, 0x0048EE20, 0x004921A4,
