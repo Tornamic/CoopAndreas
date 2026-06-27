5c97e40123816025e9c3d9b3b256c63c3d2d39ae
진짜 Claude 속도 <rosto.bayou_9f@icloud.com>
2026-06-27 10:57:19 -0300

    Add eScriptThingType enum and update CTaskSequences
    Added enumeration for script thing types and updated CTaskSequences class with new constants.

--- client/src/CTaskSequenceSync.h
+++ client/src/CTaskSequenceSync.h
@@ -1,12 +1,26 @@
 #pragma once
-class CTaskSequenceSync
+
+enum eScriptThingType : unsigned __int8
+{
+	SCRIPT_THING_SPHERE = 0x0,
+	SCRIPT_THING_EFFECT_SYSTEM = 0x1,
+	SCRIPT_THING_SEARCH_LIGHT = 0x2,
+	SCRIPT_THING_CHECKPOINT = 0x3,
+	SCRIPT_THING_SEQUENCE_TASK = 0x4,
+	SCRIPT_THING_FIRE = 0x5,
+	SCRIPT_THING_2D_EFFECT = 0x6,
+	SCRIPT_THING_DECISION_MAKER = 0x7,
+	SCRIPT_THING_PED_GROUP = 0x8,
+};
+
+
+class CTaskSequences
 {
 public:
-	static inline bool ms_bFailedToProcessSequence = false;
-	static inline std::vector<std::vector<uint8_t>> ms_vTaskSequences;
-	static bool OnOpCodeExecuted(eScriptCommands opcode);
-	static void HandlePacket(void* data, int size);
-	static bool IsNeededToCollectParametes(eScriptCommands opcode);
-	static bool IsOpCodeTaskSynced(eScriptCommands opcode);
+	static constexpr auto NUM_SEQUENCES = 64;
+	static constexpr auto NUM_TASKS = 8;
+
+	static inline int& ms_iActiveSequence = *(int*)0x8D2E98;
+	static inline bool(&ms_bIsOpened)[NUM_SEQUENCES] = *(bool(*)[NUM_SEQUENCES])0xC17898;
 };
 
