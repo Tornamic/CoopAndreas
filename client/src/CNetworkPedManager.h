49e36b1bf70ec1c1b46c340b9964fd6f98732d85
진짜 Claude 속도 <rosto.bayou_9f@icloud.com>
2026-06-27 11:01:57 -0300

    Refactor CNetworkPedManager with improved structure

--- client/src/CNetworkPedManager.h
+++ client/src/CNetworkPedManager.h
@@ -1,19 +1,175 @@
-#pragma once
-class CNetworkPedManager
+#include "stdafx.h"
+#include "CNetworkVehicle.h"
+#include "CNetworkPed.h"
+
+std::vector<CNetworkPed*> CNetworkPedManager::m_pPeds;
+CNetworkPed* CNetworkPedManager::m_apTempPeds[255];
+
+CNetworkPed* CNetworkPedManager::GetPed(int pedid)
 {
-public:
-	static std::vector<CNetworkPed*> m_pPeds;
-	static CNetworkPed* m_apTempPeds[255];
+	for (int i = 0; i != m_pPeds.size(); i++)
+	{
+		if (m_pPeds[i]->m_nPedId == pedid)
+		{
+			return m_pPeds[i];
+		}
+	}
+	return nullptr;
+}
 
-	static CNetworkPed* GetPed(int pedid);
-	static CNetworkPed* GetPed(CPed* ped);
-	static CNetworkPed* GetPed(CEntity* entity);
-	static void Add(CNetworkPed* ped);
-	static void Remove(CNetworkPed* ped);
-	static void Update();
-	static void Process();
-	static void AssignHost();
-	static unsigned char AddToTempList(CNetworkPed* networkPed);
-	static void RemoveHostedUnused();
-};
+CNetworkPed* CNetworkPedManager::GetPed(CPed* ped)
+{
+	if (ped == nullptr)
+		return nullptr;
 
+	for (int i = 0; i != m_pPeds.size(); i++)
+	{
+		if (m_pPeds[i]->m_pPed == ped)
+		{
+			return m_pPeds[i];
+		}
+	}
+
+	return nullptr;
+}
+
+CNetworkPed* CNetworkPedManager::GetPed(CEntity* entity)
+{
+	if (entity == nullptr)
+		return nullptr;
+
+	for (int i = 0; i != m_pPeds.size(); i++)
+	{
+		if (m_pPeds[i]->m_pPed == entity)
+		{
+			return m_pPeds[i];
+		}
+	}
+
+	return nullptr;
+}
+
+void CNetworkPedManager::Add(CNetworkPed* ped)
+{
+	CNetworkPedManager::m_pPeds.push_back(ped);
+}
+
+void CNetworkPedManager::Remove(CNetworkPed* ped)
+{
+	auto it = std::find(m_pPeds.begin(), m_pPeds.end(), ped);
+	if (it != m_pPeds.end())
+	{
+		m_pPeds.erase(it);
+	}
+}
+
+void CNetworkPedManager::Update()
+{
+	CNetworkPedManager::RemoveHostedUnused();
+
+	CMassPacketBuilder builder;
+
+	for (CNetworkPed* networkPed : m_pPeds)
+	{
+		if (!networkPed->m_bSyncing)
+			continue;
+
+		CPed* ped = networkPed->m_pPed;
+		if (!ped) continue;
+
+		CVehicle* vehicle = ped->m_pVehicle;
+		CNetworkVehicle* networkVehicle = vehicle ? CNetworkVehicleManager::GetVehicle(vehicle) : nullptr;
+
+		if (networkVehicle && ped->bInVehicle)
+		{
+			bool isDriver = (vehicle->m_pDriver == ped);
+
+			if (isDriver)
+			{
+				auto pedDriverUpdatePacket = CPacketHandler::PedDriverUpdate__Collect(networkVehicle, networkPed);
+				builder.AddPacket(CPacketsID::PED_DRIVER_UPDATE, pedDriverUpdatePacket, sizeof(*pedDriverUpdatePacket));
+				delete pedDriverUpdatePacket;
+			}
+			else
+			{
+				auto pedPassengerSyncPacket = CPacketHandler::PedPassengerSync__Collect(networkPed, networkVehicle);
+				builder.AddPacket(CPacketsID::PED_PASSENGER_UPDATE, pedPassengerSyncPacket, sizeof(*pedPassengerSyncPacket));
+				delete pedPassengerSyncPacket;
+			}
+		}
+		else
+		{
+			auto pedOnFootPacket = CPacketHandler::PedOnFoot__Collect(networkPed);
+			builder.AddPacket(CPacketsID::PED_ONFOOT, pedOnFootPacket, sizeof(*pedOnFootPacket));
+			delete pedOnFootPacket;
+		}
+	}
+
+	builder.Send();
+}
+
+void CNetworkPedManager::Process()
+{
+	for (auto networkPed : m_pPeds)
+	{
+		if (networkPed->m_bSyncing)
+			continue;
+
+		CPed* ped = networkPed->m_pPed;
+
+		if (ped == nullptr)
+			continue;
+
+		ped->m_fHeadingGoal = networkPed->m_fAimingRotation;
+		ped->m_fHeadingCurrent = networkPed->m_fCurrentRotation;
+		ped->m_fLookDirection = networkPed->m_fLookDirection;
+	}
+}
+
+void CNetworkPedManager::AssignHost()
+{
+	/*for (auto networkPed : m_pPeds)
+	{
+		CPed* ped = networkPed->m_pPed;
+
+		if (ped)
+		{
+			ped->SetCharCreatedBy(networkPed->m_nCreatedBy);
+
+			CTaskComplexWander* task = plugin::CallAndReturn<CTaskComplexWander*, 0x673D00>(ped); // GetWanderTaskByPedType
+			ped->m_pIntelligence->m_TaskMgr.SetTask(task, 0, false);
+		}
+	}*/
+}
+
+unsigned char CNetworkPedManager::AddToTempList(CNetworkPed* networkPed)
+{
+	for (unsigned char i = 0; i < 255; i++)
+	{
+		if (m_apTempPeds[i] == nullptr)
+		{
+			m_apTempPeds[i] = networkPed;
+			return i;
+		}
+	}
+
+	return 255;
+}
+
+void CNetworkPedManager::RemoveHostedUnused()
+{
+	for (auto it = CNetworkPedManager::m_pPeds.begin(); it != CNetworkPedManager::m_pPeds.end();)
+	{
+		if ((*it)->m_bSyncing)
+		{
+			CPed* ped = (*it)->m_pPed;
+			if (!IsPedPointerValid(ped))
+			{
+				delete* it;
+				it = m_pPeds.erase(it);
+				continue;
+			}
+		}
+		++it;
+	}
+}
