5f2d910f094160b3becd52b32d254334bfecdc18
진짜 Claude 속도 <rosto.bayou_9f@icloud.com>
2026-06-27 10:56:12 -0300

    Refactor CNetworkPlayerWaypoint into CNetworkPlayer class

--- client/src/CNetworkPlayerWaypoint.cpp
+++ client/src/CNetworkPlayerWaypoint.cpp
@@ -1,60 +1,43 @@
-#include "stdafx.h"
-
-void CNetworkPlayerWaypoint::Process()
+#pragma once
+class CNetworkPlayer
 {
-	for (auto player : CNetworkPlayerManager::m_pPlayers)
-	{
-		if (!player->m_bWaypointPlaced || player->m_vecWaypointPos == nullptr)
-			continue;
+public:
+	CPlayerPed* m_pPed = nullptr;
+	int m_iPlayerId;
 
-		CVector2D radar;
-		CRadar::TransformRealWorldPointToRadarSpace(radar, *player->m_vecWaypointPos);
-		
-		CRadar::LimitRadarPoint(radar);
+	CPackets::PlayerOnFoot m_playerOnFoot{};
+	
+	signed short m_oShockButtonL;
+	signed short m_lShockButtonL;
 
-		CVector2D screen;
-		CRadar::TransformRadarPointToScreenSpace(screen, radar);
+	CVector* m_vecWaypointPos = nullptr;
+	bool m_bWaypointPlaced = false;
 
-		CRadar::DrawRadarSprite(eRadarSprite::RADAR_SPRITE_WAYPOINT, screen.x, screen.y, 255);
+	char m_Name[32 + 1] = { 0 };
 
-		if (FrontEndMenuManager.m_bDrawRadarOrMap) 
-		{
-			const char* name = player->GetName().c_str();
-			D3DCOLOR shadowColor = D3DCOLOR_RGBA(0, 0, 0, 255);
+	CControllerState m_oldControllerState{};
+	CControllerState m_newControllerState{};
+	CCompressedControllerState m_compressedControllerState{};
 
-			// 8.0f - size of the sprite, / 2 - half of the size sprite
-			screen.x = CUtil::SCREEN_STRETCH_X(screen.x);
-			screen.y = CUtil::SCREEN_STRETCH_Y(screen.y) - CUtil::SCREEN_STRETCH_X(8.0f) / 2.f * offsetY;
+	CPackets::PlayerAimSync m_aimSyncData;
 
-			CRadar::LimitToMap(&screen.x, &screen.y);
-			RECT rect{};
-			rect.left = (LONG)screen.x;
-			rect.top = (LONG)screen.y;
-			rect.right = RsGlobal.maximumWidth;
-			rect.bottom = RsGlobal.maximumHeight;
+	CNetworkPlayerStats m_stats{};
+	CPedClothesDesc m_pPedClothesDesc{};
+	bool m_bHasBeenConnectedBeforeMe = false;
 
-			CDXFont::m_pD3DXFont->DrawTextA(nullptr, name, -1, &rect, DT_CALCRECT, D3DCOLOR_RGBA(0, 0, 0, 0));
-			int diff = (rect.right - rect.left) / 2;
-			rect.left -= diff;
-			rect.right += diff;
+	~CNetworkPlayer();
+	CNetworkPlayer(int id, CVector position);
 
-			RECT rightRect = rect;
-			rightRect.left += CDXFont::m_iShadowSize;
-			CDXFont::m_pD3DXFont->DrawTextA(nullptr, name, -1, &rightRect, 0, shadowColor);
+	void CreatePed(int id, CVector position);
+	void DestroyPed();
+	void Respawn();
+	int GetInternalId();
+	std::string GetName();
+	char GetWeaponSkill(eWeaponType weaponType);
+	void WarpIntoVehicleDriver(CVehicle* vehicle);
+	void RemoveFromVehicle(CVehicle* vehicle);
+	void WarpIntoVehiclePassenger(CVehicle* vehicle, int seatid);
+	void EnterVehiclePassenger(CVehicle* vehicle, int seatid);
+	void HandleTask(CPackets::SetPlayerTask& packet);
+};
 
-			RECT leftRect = rect;
-			leftRect.left -= CDXFont::m_iShadowSize;
-			CDXFont::m_pD3DXFont->DrawTextA(nullptr, name, -1, &leftRect, 0, shadowColor);
-
-			RECT bottomRect = rect;
-			bottomRect.top += CDXFont::m_iShadowSize;
-			CDXFont::m_pD3DXFont->DrawTextA(nullptr, name, -1, &bottomRect, 0, shadowColor);
-
-			RECT topRect = rect;
-			CDXFont::m_pD3DXFont->DrawTextA(nullptr, name, -1, &topRect, 0, shadowColor);
-			topRect.top -= CDXFont::m_iShadowSize;
-
-			CDXFont::m_pD3DXFont->DrawTextA(nullptr, name, -1, &rect, 0, D3DCOLOR_RGBA(181, 24, 24, 255));
-		}
-	}
-}
