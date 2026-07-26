90abbf7643720d023022dc8d12a8bd913d61099d
LYZENCORE-9 <tschiff6577@gmail.com>
2026-07-26 01:07:48 +0100

    support for GNU/Linux systems

--- xmake.lua
+++ xmake.lua
@@ -4,8 +4,14 @@ local force_msvc = true;
 
 set_languages("cxx17")
 set_arch("x86")
-set_plat("windows")
-set_toolchains("clang-cl") -- required to generate compile_commands.json properly
+
+if is_os("windows") then
+    set_plat("windows")
+    set_toolchains("clang-cl") -- required to generate compile_commands.json properly
+else
+    set_plat("linux")
+    --set_toolchains("clang") -- it's better to let abilitiy to use GNU GCC or LLVM Clang toolchains on GNU/Linux systems
+end
 
 add_rules("plugin.compile_commands.autoupdate")
 add_rules("mode.debug", "mode.release")
@@ -18,86 +24,89 @@ if is_os("windows") then
     end
 end
 
-target("client")
-    set_kind("shared")
-    if force_msvc == true then
-        set_toolchains("msvc")
-    end
-    set_basename("CoopAndreasSA")
+if is_os("windows") then
 
-    local gta_sa_dir = os.getenv("GTA_SA_DIR")
-    if gta_sa_dir then
-        set_targetdir(gta_sa_dir)
-    end
+    target("client")
+        set_kind("shared")
+        if force_msvc == true then
+            set_toolchains("msvc")
+        end
+        set_basename("CoopAndreasSA")
 
-    set_arch("x86")
-    set_plat("windows")
-    
-    add_files("client/src/*.cpp")
-    add_files("client/src/Commands/*.cpp")
-    add_files("client/src/Commands/Commands/*.cpp")
-    add_files("client/src/game_sa/*.cpp")
-    add_files("client/src/Hooks/*.cpp")
-
-    add_headerfiles("client/src/*.h")
-    add_headerfiles("client/src/Commands/*.h")
-    add_headerfiles("client/src/Commands/Commands/*.h")
-    add_headerfiles("client/src/game_sa/*.h")
-    add_headerfiles("client/src/Hooks/*.h")
-
-    add_includedirs("client/src")
-
-    add_files("third_party/enet/*.c")
-    add_files("third_party/DiscordRPC/SDK/src/*.cpp")
-
-    set_pcxxheader("client/src/stdafx.h")
-
-    add_includedirs("shared", "third_party")
-    add_includedirs("third_party/DiscordRPC/SDK/include")
-
-    add_syslinks("kernel32", "user32", "ws2_32", "winmm", "Advapi32")
-    
-    add_files("client/version.rc")
-
-    add_defines("_CRT_SECURE_NO_WARNINGS", "_CRT_NON_CONFORMING_SWPRINTFS", "_USE_MATH_DEFINES", "_SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING", "GTASA", "PLUGIN_SGV_10US", "RW")
-
-    if is_mode("debug") then
-        add_defines("DEBUG")
-    else
-        add_defines("NDEBUG")
-        set_strip("all")
-        set_optimize("fastest")
-    end
-
-    on_load(function (target)
-        local plugin_sdk = os.getenv("PLUGIN_SDK_DIR")
-        if not plugin_sdk then
-            print("Skipping client target because PLUGIN_SDK_DIR is not set")
-            return
+        local gta_sa_dir = os.getenv("GTA_SA_DIR")
+        if gta_sa_dir then
+            set_targetdir(gta_sa_dir)
         end
 
-        target:add("includedirs", 
-            path.join(plugin_sdk, "shared"), 
-            path.join(plugin_sdk, "shared/game"), 
-            path.join(plugin_sdk, "shared/dxsdk"), 
-            path.join(plugin_sdk, "plugin_sa"),
-            path.join(plugin_sdk, "plugin_sa/game_sa")
-        )
+        set_arch("x86")
+        set_plat("windows")
+    
+        add_files("client/src/*.cpp")
+        add_files("client/src/Commands/*.cpp")
+        add_files("client/src/Commands/Commands/*.cpp")
+        add_files("client/src/game_sa/*.cpp")
+        add_files("client/src/Hooks/*.cpp")
 
-        target:add("linkdirs", 
-            path.join(plugin_sdk, "output/lib"),
-            path.join(plugin_sdk, "shared/bass"),
-            path.join(plugin_sdk, "shared/dxsdk")
-        )
+        add_headerfiles("client/src/*.h")
+        add_headerfiles("client/src/Commands/*.h")
+        add_headerfiles("client/src/Commands/Commands/*.h")
+        add_headerfiles("client/src/game_sa/*.h")
+        add_headerfiles("client/src/Hooks/*.h")
 
-        if is_mode("debug") then 
-            target:add("links", "plugin_d")
-        else 
-            target:add("links", "plugin")
+        add_includedirs("client/src")
+
+        add_files("third_party/enet/*.c")
+        add_files("third_party/DiscordRPC/SDK/src/*.cpp")
+
+        set_pcxxheader("client/src/stdafx.h")
+
+        add_includedirs("shared", "third_party")
+        add_includedirs("third_party/DiscordRPC/SDK/include")
+
+        add_syslinks("kernel32", "user32", "ws2_32", "winmm", "Advapi32")
+    
+        add_files("client/version.rc")
+
+        add_defines("_CRT_SECURE_NO_WARNINGS", "_CRT_NON_CONFORMING_SWPRINTFS", "_USE_MATH_DEFINES", "_SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING", "GTASA", "PLUGIN_SGV_10US", "RW")
+
+        if is_mode("debug") then
+            add_defines("DEBUG")
+        else
+            add_defines("NDEBUG")
+            set_strip("all")
+            set_optimize("fastest")
         end
 
-        target:add("links", "d3dx9")
-    end)
+        on_load(function (target)
+            local plugin_sdk = os.getenv("PLUGIN_SDK_DIR")
+            if not plugin_sdk then
+                print("Skipping client target because PLUGIN_SDK_DIR is not set")
+                return
+            end
+
+            target:add("includedirs", 
+                path.join(plugin_sdk, "shared"), 
+                path.join(plugin_sdk, "shared/game"), 
+                path.join(plugin_sdk, "shared/dxsdk"), 
+                path.join(plugin_sdk, "plugin_sa"),
+                path.join(plugin_sdk, "plugin_sa/game_sa")
+            )
+
+            target:add("linkdirs", 
+                path.join(plugin_sdk, "output/lib"),
+                path.join(plugin_sdk, "shared/bass"),
+                path.join(plugin_sdk, "shared/dxsdk")
+            )
+
+            if is_mode("debug") then 
+                target:add("links", "plugin_d")
+            else 
+                target:add("links", "plugin")
+            end
+
+            target:add("links", "d3dx9")
+        end)
+end
 
 target("server")
     set_kind("binary")
@@ -130,30 +139,32 @@ target("server")
         add_files("server/version.rc")
         add_defines("_CRT_SECURE_NO_WARNINGS", "WIN32", "_CONSOLE")
 
-    elseif is_os("linux") then
-        -- TODO
+    -- elseif is_os("linux") then               better to remove this elseif condition cause Xmake will use the default compiler on linux GCC or LLVM
+    --    set_toolchains("clang") 
     end
 
-target("proxy")
-    set_kind("shared")
-    if force_msvc == true then
-        set_toolchains("msvc")
-    end
-    set_arch("x86")
-    set_plat("windows")
+if is_os("windows") then
+    target("proxy")
+        set_kind("shared")
+        if force_msvc == true then
+            set_toolchains("msvc")
+        end
+        set_arch("x86")
+        set_plat("windows")
 
-    add_files("proxy/src/*.cpp")
-    add_headerfiles("proxy/src/*.h")
+        add_files("proxy/src/*.cpp")
+        add_headerfiles("proxy/src/*.h")
     
-    add_syslinks("kernel32", "user32")
+        add_syslinks("kernel32", "user32")
 
-    add_defines("PROXY_EXPORTS", "_WINDOWS", "_USRDLL")
+        add_defines("PROXY_EXPORTS", "_WINDOWS", "_USRDLL")
 
-    if is_mode("debug") then
-        add_defines("_DEBUG")
-        set_symbols("debug")
-    else
-        add_defines("NDEBUG")
-        set_optimize("fastest")
-        set_strip("all")
-    end
+        if is_mode("debug") then
+            add_defines("_DEBUG")
+            set_symbols("debug")
+        else
+            add_defines("NDEBUG")
+            set_optimize("fastest")
+            set_strip("all")
+        end
+end
