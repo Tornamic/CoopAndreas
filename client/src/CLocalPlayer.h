8eb5c02292f71e698ebb688162ebe907493d6f3e
EOS <68227685+NoPressF@users.noreply.github.com>
2026-07-27 09:51:17 +0200

    Rework player list (#91)
    * Rework player list
    
    * Set display players limit to MAX_SERVER_PLAYERS
    
    * minor improvements
    
    ---------
    
    Co-authored-by: Tornamic <kirilltymoshchenko59@gmail.com>

--- client/src/CLocalPlayer.h
+++ client/src/CLocalPlayer.h
@@ -4,7 +4,7 @@ class CLocalPlayer
 public:
 	static inline float m_vecLastAimX;
 	static inline float m_vecLastAimY;
-	static inline char m_Name[32 + 1];
+	static inline char m_Name[Config::MAX_NICKNAME_LENGTH + 1];
 	static inline bool m_bIsHost;
 
 	static void BuildTaskPacket(eTaskType type, bool toggle = true);
--- client/src/CUtil.h
+++ client/src/CUtil.h
@@ -4,6 +4,9 @@ class CUtil
 {
 public:
 	static constexpr float DEFAULT_ASPECT_RATIO = 4.0f / 3.0f;
+	static constexpr float SCREEN_BASE_WIDTH = 640.0f;
+	static constexpr float SCREEN_BASE_HEIGHT = 448.0f;
+
 	static bool CompareControllerStates(const CControllerState& state1, const CControllerState& state2);
 	static void CopyControllerState(CControllerState& destination, const CControllerState& source);
 	static bool IsDucked(CPed* ped);
@@ -18,11 +21,11 @@ public:
 	static bool IsPedHasJetpack(CPed* ped);
 	static void SetPlayerJetpack(CNetworkPlayer* ped, bool set);
 	static std::string GetWeaponName(eWeaponType type);
-	static inline float SCREEN_STRETCH_X(float a) { return a * RsGlobal.maximumWidth / (float)640; }
-	static inline float SCREEN_STRETCH_Y(float a) { return a * RsGlobal.maximumHeight / (float)448; }
-	static inline float SCREEN_SCALE_AR(float a) { return a * DEFAULT_ASPECT_RATIO / CDraw::ms_fAspectRatio; };
-	static inline float SCREEN_SCALE_X(float a) { return SCREEN_SCALE_AR(SCREEN_STRETCH_X(a)); }
-	static inline float SCREEN_SCALE_Y(float a) { return SCREEN_STRETCH_Y(a); }
+	static float SCREEN_STRETCH_X(float a) { return a * RsGlobal.maximumWidth / SCREEN_BASE_WIDTH; }
+	static float SCREEN_STRETCH_Y(float a) { return a * RsGlobal.maximumHeight / SCREEN_BASE_HEIGHT; }
+	static float SCREEN_SCALE_AR(float a) { return a * DEFAULT_ASPECT_RATIO / CDraw::ms_fAspectRatio; };
+	static float SCREEN_SCALE_X(float a) { return SCREEN_SCALE_AR(SCREEN_STRETCH_X(a)); }
+	static float SCREEN_SCALE_Y(float a) { return SCREEN_STRETCH_Y(a); }
 
 };
 
